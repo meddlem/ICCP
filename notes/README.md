04690d38e97b0648b7cac3f75bd44a7ecf78e15c
meddlem <mprebergen@gmail.com>
2015-02-11 23:32:06 +0100

    small fixes2

--- notes/README.md
+++
@@ -1,3 +0,0 @@
-# Coding Notes
-
-For pdf file of these notes go [here](https://github.com/ICCP/coding-notes/releases/download/2015.0/coding_notes.pdf)
--- notes/examples/smart_sum_function.f90
+++
@@ -1,8 +0,0 @@
-! Gauss was a genius! 1 + 2 + 3 + ... + n = n*(n + 1) / 2
-integer function smart_sum(a, b) result(total)
-
-    integer, intent(in) :: a, b
-
-    total = (b * (b + 1)) / 2 - (a * (a - 1)) / 2
-
-end function
