d8df8ac56ab58f2d58d06c4b3b9d09a0f263957d
meddlem <mprebergen@gmail.com>
2015-02-14 16:09:17 +0100

    parallel do removed, allocation also

--- Interactions.f95
+++ Interactions.f95
@@ -1,5 +1,4 @@
 module Interactions
-  use omp_lib
   implicit none
   private 
   public Force
@@ -8,16 +7,16 @@ contains
   subroutine Force(F,EV,r,rc,L)
     real(8), intent(in) :: rc, L, r(:,:)
     real(8), intent(out) :: F(:,:), EV
-    real(8) :: d, dr(3) 
-    real(8), allocatable :: FMAT(:,:,:)
+    real(8) :: d, dr(3), FMAT(size(r,1),size(r,1),3)
+    ! real(8), allocatable :: FMAT(:,:,:)!, VMAT(:,:)
     integer :: i, j, N
 
     ! initialize, allocate large array
     N = size(r,1)
     EV = 0d0
-    allocate(FMAT(N,N,3))
+    FMAT(:,:,:) = 0
 
-    !$omp parallel do private(dr, d, i, j)
+    !$# omp parallel do private(dr, d)
     do i = 1,N
       j = 1
       do while (j<i)
@@ -27,22 +26,26 @@ contains
 
         if (d>(sqrt(3d0)*L)) then !check
           print *, "warning: reduce dt", d
-          stop
         endif
 
         if (d<rc) then ! only particle pairs with d<rc contribute to E, F
           FMAT(i,j,:) = 48d0*dr*(1d0/(d**14d0)-0.5d0/(d**8d0))
+          ! VMAT(i,j) = 4d0*(1d0/(d**12d0)-1d0/(d**6d0))
           EV = EV + 4d0*(1d0/(d**12d0)-1d0/(d**6d0))
         else
           FMAT(i,j,:) = 0d0
+          ! VMAT(j,i) = 0d0
         endif
 
-        FMAT(j,i,:) = - FMAT(i,j,:) !use N3
+        FMAT(j,i,:) = - FMAT(i,j,:) ! use N3
+        ! VMAT(j,i) = 0d0 
         j = j+1
       enddo
     enddo
-    !$omp end parallel do
-    F = sum(FMAT,2) !calculate total force vector on particle i
-    deallocate(FMAT)
+
+    F = sum(FMAT,2) ! calculate total force vector on particle i
+    ! EV = sum(VMAT) ! total interaction energy
+    ! deallocate(FMAT)
+    ! deallocate(VMAT)
   end subroutine Force
 end module 
--- main.f95
+++ main.f95
@@ -8,19 +8,19 @@ program main
 
   ! model parameters (constants):
   ! dt = timestep, rc = LJ potential cutoff length, Tinit = inital temp, &
-  ! m = mass, L=length, units: eps=1, sigma=1, m=1, &
+  ! m = mass, rho = number density, units: eps=1, sigma=1, m=1, &
   ! steps = number of timesteps, N = number of particles, in FCC lattice 
-  real(8), parameter :: dt = 0.001d0, rc = 2.5d0, Tinit = 1d0, L = 14d0, & 
+  real(8), parameter :: dt = 0.001d0, rc = 2.5d0, Tinit = 1d0, rho = 0.5d0, & 
     eps = 1d0, sigma = 1d0, m = 1d0  
-  integer, parameter :: steps = 1000, N = 7**3*4
+  integer, parameter :: steps = 1000, N = 6**3*4
   
-  ! axislabels in plot:
+  ! axis labels in plot:
   character(10), parameter :: xlabel = "time", ylabel = "T", label = "plot"   
   
   ! declare variables:
   real(8) :: x(steps+1), xrange(2), yrange(2), T(steps+1), E(steps+1), EV, &
-    r(N,3), p(N,3), F(N,3) 
-  integer :: i
+    r(N,3), p(N,3), F(N,3), L = (N/rho)**(1./3.) 
+  integer :: i, starttime, endtime
   
   ! initialize r, p, F, and 3d plot:
   call InitR(r,L,N)
@@ -36,7 +36,8 @@ program main
   print *, "Psum t=0 : ", sum(p,1) 
   print *, "E t=0 : ", E(1)/N 
   print *, "T t=0 : ", T(1)
-
+  call system_clock(starttime)
+  
   ! time integration using the "velocity Verlet" algorithm: 
   do i = 1,steps
     ! plot particle positions
@@ -52,9 +53,10 @@ program main
     T(i+1) = sum(p**2)/N
     E(i+1) = EV + 0.5d0*N*T(i+1)
   enddo
-
+  call system_clock(endtime)
   call plend()
 
+  print *, "runtime =", endtime-starttime
   print *, "Psum final: ", sum(p,1)
   print *, "E final: ", E(steps+1)/N 
   print *, "T final: ", T(steps+1)
