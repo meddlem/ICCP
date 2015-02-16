0a087048318083bce261cca5dc1c9fb0e37dcc37
meddlem <mprebergen@gmail.com>
2015-02-16 23:23:51 +0100

    still promising

--- main.f95
+++ main.f95
@@ -11,8 +11,8 @@ program main
   ! dt = timestep, rc = LJ potential cutoff length, T_init = inital temp, &
   ! m = mass, rho = number density, units: eps=1, sigma=1, m=1, &
   ! steps = number of timesteps, N = number of particles, in FCC lattice 
-  real(8), parameter :: dt = 0.001d0, rc = 2.5d0, T_init = 1.585d0, & 
-    rho = 0.65d0,  pi = 4d0*atan(1d0)
+  real(8), parameter :: dt = 0.001d0, rc = 2.5d0, T_init = 1.095d0, & 
+    rho = 0.88d0,  pi = 4d0*atan(1d0)
   integer, parameter :: steps = 3600, N = 6**3*4
   logical, parameter :: prtplt = .false.
   real(8), parameter :: L = (N/rho)**(1./3.)
